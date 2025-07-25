d04b533882ff7849df4fef55ce2f35a68e04035b
Bo Lemmers <bo.lemmers@gmail.com>
2025-07-25 15:37:17 +0200

    fixed the output for decimals

--- dbt_diagrams/domain.py
+++ dbt_diagrams/domain.py
@@ -65,10 +65,14 @@ class Column(BaseModel, **PYDANTIC_MODEL_CONFIG):
             # ARRAY<STRUCT<a INT64, b NUMERIC>>
             # We need to remove the content of the struct type
             # because it's not allowed by Mermaid syntax.
-            cleaned_struct_type = re.sub("(.*STRUCT<)(.*?)(>.*)", r"\1\3", self.type)
-            return cleaned_struct_type.replace("<", "[").replace(">", "]")
-        else:
-            return self.type
+            if "STRUCT" in self.type:
+                cleaned_struct_type = re.sub("(.*STRUCT<)(.*?)(>.*)", r"\1\3", self.type)
+                return cleaned_struct_type.replace("<", "[").replace(">", "]")
+            elif "DECIMAL" in self.type.upper():  # Check uppercase version
+                return re.sub(r"DECIMAL\([^)]*\)", "decimal", self.type, flags=re.IGNORECASE)
+            else:
+                return self.type
+
 
     @classmethod
     def from_manifest_catalog_node_columns(
